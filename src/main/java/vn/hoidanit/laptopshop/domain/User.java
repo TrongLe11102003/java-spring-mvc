2210a95c7b8517b4dc848c179b5b644e1e922e1f
Lê Văn Trọng <tronglvhe173586@fpt.edu.vn>
2026-03-04 21:57:11 +0700

    jsp-html-bootstrap

---
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -0,0 +1,67 @@
+package vn.hoidanit.laptopshop.domain;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String fullName;
+    private String address;
+    private String phone;
+
+    
+    public User() {
+    }
+    
+    public User(Long id, String email, String password, String fullName, String address, String phone) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getphone() {
+        return phone;
+    }
+    public void setphone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + "]";
+    }
+    
+}
