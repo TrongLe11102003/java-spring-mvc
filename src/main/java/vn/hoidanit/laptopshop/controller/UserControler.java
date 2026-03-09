a4dfbeb51edbdff26e86762cf8a1b455288367bd
Lê Văn Trọng <tronglvhe173586@fpt.edu.vn>
2026-03-09 18:47:11 +0700

    Chi-tiet-user

--- src/main/java/vn/hoidanit/laptopshop/controller/UserControler.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserControler.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,17 +38,34 @@ public class UserControler {
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
-        User user = new User();
-        model.addAttribute("newUser", user);
-        return "admin/user/create";
+        List<User> users = this.userService.getAllUsers();
+        model.addAttribute("users1", users);
+        System.out.println(">>> check users:" + users);
+        return "admin/user/table-user";
     }
+
+     @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        System.out.println("check path id = " + id);
+        model.addAttribute("id", id);
+        return "/admin/user/show";
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String createUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "/admin/user/create";
+    }  
+
     
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
         System.out.println("run here" + hoidanit);
         this.userService.handleSaveUser(hoidanit);
-        return "hello";
-    }   
+        return "redirect:/admin/user";
+    }  
+    
+    
 }
 
 // @RestController
